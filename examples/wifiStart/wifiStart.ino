1834c37eca1c9d2fb1df6ef6f5dc2be033c616c3
Сергей Третьяков <tretyakov.sa@gmail.com>
2021-11-09 00:58:08 +0700

    Пример сброса

--- examples/wifiStart/wifiStart.ino
+++ examples/wifiStart/wifiStart.ino
@@ -8,11 +8,16 @@ const String ssidApPass = "";
 void setup() {
   Serial.begin(115200);
   Serial.println();
+  MyWiFi.restoreCallback( []() { // будет выполнен как только кнопка Reset будет нажата n раз
+    Serial.println("Rerstore" ); // запустите свою функцию сброса к заводским настройкам
+    });
+	MyWiFi.beginRestore(5); // запустим проверку многократного нажатия Reset n раз
   MyWiFi.init(ssid, ssidPass, ssidAP, ssidApPass); // Основные данные для wifi
   MyWiFi.start();        // Запустим WiFi
   if (MyWiFi.modeSTA()) { // Если подключились
     Serial.println("Connect "+ssid);
   } else  Serial.println("AP ="+ssidAP);
+  MyWiFi.endRestore(); // сбросим счетчик Restore в ноль
 }
 
 void loop() {
